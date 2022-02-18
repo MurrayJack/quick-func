c5d926e91355713ef0ef4e2039cddb86feb92e86
Murray Jack <mjack@starrez.com>
2022-02-18 18:29:45 +1100

    Update guid.js

--- src/pages/functions/guid.js
+++ src/pages/functions/guid.js
@@ -2,17 +2,17 @@ import * as React from "react"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 
-function buildGuid() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16)
-  )
-}
-
 const IndexPage = () => {
   const [guid, setGuid] = React.useState(buildGuid())
+
+  const buildGuid = () => {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    )
+  }
 
   return (
     <Layout>
