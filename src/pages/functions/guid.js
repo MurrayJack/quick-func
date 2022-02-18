424f070211e8b21cb210353fb6b6d221508adb1c
Murray Jack <mjack@starrez.com>
2022-02-18 18:19:34 +1100

    push latest

---
+++ src/pages/functions/guid.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+
+function buildGuid() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  )
+}
+
+const IndexPage = () => {
+  const [guid, setGuid] = React.useState(buildGuid())
+
+  return (
+    <Layout>
+      <Seo title="Create GUID" />
+
+      <input type="text" readOnly value={guid}></input>
+
+      <button onClick={() => setGuid(buildGuid())}>do it</button>
+
+      <textarea>
+        {`function buildGuid() {
+            return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+            );
+        }`}
+      </textarea>
+    </Layout>
+  )
+}
+
+export default IndexPage
